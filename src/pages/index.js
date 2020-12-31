a045fe1cf1e3c0d68c1e9054229b7558e9a990c2
bharathreddyvanteru <76803710+bharathreddyvanteru@users.noreply.github.com>
2020-12-31 15:09:31 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,12 +24,11 @@ const BlogIndex = ({ data, location }) => {
   }
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <Layout location={location} title={"Best CRM Softwares}>
       <SEO title="Best crm softwares" />
       <h1> What are the best CRM softwares ? </h1>
       <p> Tired of figuring out best crm softwares, check this 
-  <a className="text-4xl font-bold text-yellow-600 font-display" href= "https://techcentred.com/top-5-best-crm-software-for-every-business/"> best crm softwares</a> 
-  list curated by  <a href="https://techcentred.com">TechCentred</a></p>
+  <a className="text-4xl font-bold text-yellow-600 font-display" href= "https://techcentred.com/top-5-best-crm-software-for-every-business/"> best crm softwares</a> list curated by  <a href="https://techcentred.com">TechCentred</a></p>
 
     </Layout>
   )
